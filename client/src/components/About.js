7afe7f3671ab4c8a4456643c27fb6241b43cc3a0
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2022-02-09 09:41:06 +0100

    test about section added

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,6 +4,12 @@ import React from "react";
 
 export default function About() {
     return (
-        <p>hello</p>
+        <p>
+        
+        As a new developer I am looking first and foremost to be a part of a quality oriented team that prioritizes efficiency and teamwork, where i can acquire new skills and habits from my coworkers. I believe mutual respect and understanding is fundamental for any team and I always try to add to this with an open disposition and good humor.  
+        As a person I am detail-oriented, inquisitive and happy. I enjoy working on complicated challenges with other skilled developers. During both my education and internship I have had the privilege of working with a large set of different people from different background from whom I have learned a lot.
+        
+        I have solid experience with Java, SQL, full stack JavaScript as well UP and Scrum to name a few. From my internship at a smaller startup I bring practical experience with TypeScript, PostgreSQL, React, MongoDB, Docker and more. I have a strong foundational knowledge of principals of design and patterns which means that I easily can learn the details of new languages, tools and frameworks. I like to learn new things and always aim to write clean and future proof code in any language.
+        </p>
       );
 }
